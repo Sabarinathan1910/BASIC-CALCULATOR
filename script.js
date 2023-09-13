88238a3e3c5653e8820d475eab11272039ffb675
Sabarinathan1910 <138775462+Sabarinathan1910@users.noreply.github.com>
2023-09-13 15:42:07 +0530

    added input fucntionalities

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-const display = document.getElementById('display');
-const buttons = document.querySelectorAll('button[data-value]');
-const clearButton = document.getElementById('clear');
-const calculateButton = document.getElementById('calculate');
+ const display = document.getElementById('display');
+    const buttons = document.querySelectorAll('button[data-value]');
+    const clearButton = document.getElementById('clear');
+    const calculateButton = document.getElementById('calculate');
 
-// Add click event listeners to number and operator buttons
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    display.value += button.dataset.value;
-  });
-});
+    // Add click event listeners to number and operator buttons
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        display.value += button.dataset.value;
+      });
+    });
 
-// Clear the display
-clearButton.addEventListener('click', () => {
-  display.value = '';
-});
+    // Clear the display
+    clearButton.addEventListener('click', () => {
+      display.value = '';
+    });
 
-// Calculate and display the result
-calculateButton.addEventListener('click', () => {
-  try {
-    const result = eval(display.value);
-    display.value = result;
-  } catch (error) {
-    display.value = 'Error';
-  }
-});
+    // Calculate and display the result
+    calculateButton.addEventListener('click', () => {
+      try {
+        const result = eval(display.value);
+        display.value = result;
+      } catch (error) {
+        display.value = 'Error';
+      }
+    });
+    document.getElementById("display").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            calculate();
+        }
+    });
+    
+    function calculate() {
+        try {
+            const input = document.getElementById("display").value;
+            const result = eval(input);
+            document.getElementById("display").value = result;
+        } catch (error) {
+            // Handle invalid expressions
+            document.getElementById("display").value = " ";
+        }
+    }
+    
